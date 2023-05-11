6e9dba6cb1deaeb43095c0f799697a04814d412c
BenGregory23 <bengregorygbr@gmail.com>
2023-05-11 11:26:58 +0200

    added config to fix routing error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copy } from 'fs-extra';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    // Copy the _redirects file to the build output directory
+    afterBuild: async () => {
+      await copy('_redirects', 'dist/_redirects');
+    },
+  },
 })
