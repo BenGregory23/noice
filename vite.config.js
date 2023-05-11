490ff667e4755868b9060ae2dc18936a87348dab
BenGregory23 <bengregorygbr@gmail.com>
2023-05-11 11:35:25 +0200

    Removed old config and file and trying new fix

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
-import { copyFileSync } from 'fs';
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  // Hook to copy the _redirects file after build
-  afterBuild: () => {
-    const redirectsSource = resolve(__dirname, '_redirects');
-    const redirectsDestination = resolve(__dirname, 'dist', '_redirects');
-    copyFileSync(redirectsSource, redirectsDestination);
-  },
 })
