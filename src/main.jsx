a57151529f15f35432c3273f5239d14d55d1361b
BenGregory23 <bengregorygbr@gmail.com>
2023-09-11 20:26:21 +0200

    removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import '../src/css/loader.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>,
+  
 )
